bf48faa5880a1dc41491e57090226e0236069bc0
Nghi Le Thanh <nghi.le@tuni.fi>
2020-11-28 15:36:01 +0100

    Add test for words js

---
+++ test/words.test.js
@@ -0,0 +1,36 @@
+import words from '../src/words.js';
+import chai from 'chai';
+
+let expect = chai.expect;
+let assert = chai.assert;
+
+describe('Test word.js function', () => {
+
+    it('Give null to method, method is failed because match method of String is missing', () => {
+        expect(() => {
+            words(null, null);
+        }).to.throw(TypeError);
+    });
+
+    it('Give undefined to method, method is failed because match method of String is missing', () => {
+        expect(() => {
+            words(undefined, null);
+        }).to.throw(TypeError);
+    });
+
+    it('Give empty string without pattern, return empty array', () => {
+        expect(words('')).to.be.instanceOf(Array).that.eql([]);
+    });
+
+    it('Give sample valid string, return array with splitted words and remove all special chars', () => {
+        expect(words('abc.a abc abb')).to.be.instanceOf(Array).that.eql(['abc', 'a', 'abc', 'abb']);
+        expect(words('abc.a a,bc ab|b')).to.be.instanceOf(Array).that.eql(['abc', 'a', 'a', 'bc', 'ab', 'b']);
+    });
+
+    it('Give sample valid string, return array with splitted words match patterns', () => {
+        const SPLIT_WORDS_BY_SPACE_AND_COMMA = /[^, ]+/g;
+        expect(words('abc.a ab,c abb', SPLIT_WORDS_BY_SPACE_AND_COMMA))
+                .to.be.instanceOf(Array)
+                .that.eql(['abc.a', 'ab', 'c', 'abb']);
+    });
+});
