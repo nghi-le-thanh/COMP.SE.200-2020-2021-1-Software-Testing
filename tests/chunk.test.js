bb3b566fd735d6ff3c9895170519e2497656b121
nghi.thanh <nghi.thanh@valtech.com>
2020-12-03 22:02:23 +0100

    Fix chunk coverage

--- tests/chunk.test.js
+++ tests/chunk.test.js
@@ -7,16 +7,12 @@ const assert = chai.assert;
 describe('chunk.js test', () => {
     // expect fail test
     it('chunk a valid array with zero size, expect return unchanged array', () => {
-        try {
-            assert.deepEqual(chunk([1,2,3], 0), [1,2,3]);
-        } catch (error) {
-            console.warn('Fail test chunk a valid array with zero size, expect return unchanged array. Reason: ', error.message);
-        }
+        assert.deepEqual(chunk([1, 2, 3], 0), [1, 2, 3]);
     });
 
     // expect fail test
     it('chunk a valid array with negavitve size, expect empty array', () => {
-        assert.deepEqual(chunk([1,2,3], -100.55), []);
+        assert.deepEqual(chunk([1, 2, 3], -100.55), []);
     });
 
     it('chunk an empty array with valid size, expect empty array', () => {
@@ -29,21 +25,22 @@ describe('chunk.js test', () => {
 
     // expect fail test
     it('chunk invalid type with valid size, expect thrown error', () => {
-        try {
-            assert.throws(() => {
-                chunk("hello world", 2)
-            }, 'Invalid input');    
-        } catch (error) {
-            console.warn('chunk invalid type with valid size, expect thrown error. Reason: ', error.message);
-        }
+        assert.throws(() => {
+            chunk("hello world", 2)
+        }, 'Invalid input');    
     });
 
     // unexpected fail test
     it('chunk valid type with valid size, expect valid sub arrays', () => {
-        try {
-            assert.deepEqual(chunk(['foo', 'doctor', 'hello'], 2), [['foo', 'doctor'], ['hello']]);
-        } catch(error) {
-            console.warn('Unexpected fail test chunk valid type with valid size, expect valid sub arrays. Reason: ', error.message);
-        }
+        assert.deepEqual(chunk(['foo', 'doctor', 'hello'], 2), [['foo', 'doctor'], ['hello']]);
     });
+
+    it('chunk an array without explicitly size, default size is 1', () => {
+        assert.deepEqual(chunk(['a', 'b', 'c', 'd']), [['a'], ['b'], ['c'], ['d']]);
+    });
+
+    it('chunk a null, then expect return empty array, discard size', () => {
+        assert.deepEqual(chunk(null, 2), []);
+    });
+
 });
